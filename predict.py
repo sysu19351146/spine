2144c8c27566e95e9d37ffe6648abab02b178744
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2021-05-23 23:02:41 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,9 @@
-#-------------------------------------#
-#       对单张图片进行预测
-#-------------------------------------#
 from yolo import YOLO
 from PIL import Image
 
 
 
-
+#使用yolo模型对图片进行检测
 def show():
     yolo = YOLO(model_path="logs/pre.pth",classes_path="model_data/new_classes.txt",max_box=13)
     test_path="./testing_annotation_final.txt"
