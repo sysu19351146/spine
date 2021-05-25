ebb93b420ff415de778f41226d9bd40a33888936
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2021-05-25 21:43:01 +0800

    Update utils.py

--- net_base/util/utils.py
+++ net_base/util/utils.py
@@ -138,73 +138,6 @@ def bbox_iou(box1, box2, x1y1x2y2=True):
     return iou
 
 
-def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
-    # 求左上角和右下角
-    box_corner = prediction.new(prediction.shape)
-    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
-    box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
-    box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
-    box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
-    prediction[:, :, :4] = box_corner[:, :, :4]
-
-    output = [None for _ in range(len(prediction))]
-    for image_i, image_pred in enumerate(prediction):
-        # 获得种类及其置信度
-        class_conf, class_pred = torch.max(image_pred[:, 5:5 + num_classes], 1, keepdim=True)
-
-        # 利用置信度进行第一轮筛选
-        conf_mask = (image_pred[:, 4]*class_conf[:, 0] >= conf_thres).squeeze()
-
-        image_pred = image_pred[conf_mask]
-        class_conf = class_conf[conf_mask]
-        class_pred = class_pred[conf_mask]
-        if not image_pred.size(0):
-            continue
-        # 获得的内容为(x1, y1, x2, y2, obj_conf, class_conf, class_pred)
-        detections = torch.cat((image_pred[:, :5], class_conf.float(), class_pred.float()), 1)
-
-        # 获得种类
-        unique_labels = detections[:, -1].cpu().unique()
-
-        if prediction.is_cuda:
-            unique_labels = unique_labels.cuda()
-            detections = detections.cuda()
-
-        for c in unique_labels:
-            # 获得某一类初步筛选后全部的预测结果
-            detections_class = detections[detections[:, -1] == c]
-
-            #------------------------------------------#
-            #   使用官方自带的非极大抑制会速度更快一些！
-            #------------------------------------------#
-            # keep = nms(
-            #     detections_class[:, :4],
-            #     detections_class[:, 4]*detections_class[:, 5],
-            #     nms_thres
-            # )
-            # max_detections = detections_class[keep]
-            
-            # # 按照存在物体的置信度排序
-            _, conf_sort_index = torch.sort(detections_class[:, 4]*detections_class[:, 5], descending=True)
-            detections_class = detections_class[conf_sort_index]
-            # 进行非极大抑制
-            max_detections = []
-            while detections_class.size(0):
-                # 取出这一类置信度最高的，一步一步往下判断，判断重合程度是否大于nms_thres，如果是则去除掉
-                max_detections.append(detections_class[0].unsqueeze(0))
-                if len(detections_class) == 1:
-                    break
-                ious = bbox_iou(max_detections[-1], detections_class[1:])
-                detections_class = detections_class[1:][ious < nms_thres]
-            # 堆叠
-            max_detections = torch.cat(max_detections).data
-            
-            # Add max detections to outputs
-            output[image_i] = max_detections if output[image_i] is None else torch.cat(
-                (output[image_i], max_detections))
-
-    return output
-
 def resize_pre(prediction):
     box_corner = prediction.new(prediction.shape)
     box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
@@ -246,29 +179,13 @@ def non_max_suppression2(prediction, num_classes,max_box,conf_thres=0.5, nms_thr
             # 获得某一类初步筛选后全部的预测结果
             detections_class = detections[detections[:, -1] == c]
 
-            # ------------------------------------------#
-            #   使用官方自带的非极大抑制会速度更快一些！
-            # ------------------------------------------#
+            #非极大性抑制
             keep = nms(
                 detections_class[:, :4],
                 detections_class[:, 4] * detections_class[:, 5],
                 nms_thres
             )
             max_detections = detections_class[keep]
-            # _, conf_sort_index = torch.sort(detections_class[:, 4]*detections_class[:, 5], descending=True)
-            # detections_class = detections_class[conf_sort_index]
-            # # 进行非极大抑制
-            # max_detections = []
-            # while detections_class.size(0):
-            #     # 取出这一类置信度最高的，一步一步往下判断，判断重合程度是否大于nms_thres，如果是则去除掉
-            #     max_detections.append(detections_class[0].unsqueeze(0))
-            #     if len(detections_class) == 1:
-            #         break
-            #     ious = bbox_iou(max_detections[-1], detections_class[1:])
-            #     detections_class = detections_class[1:][ious < nms_thres]
-            # 堆叠
-            # max_detections = torch.cat(max_detections).data
-
             output[image_i] = max_detections if output[image_i] is None else torch.cat(
                 (output[image_i], max_detections))
     if output[0]==None:
@@ -278,7 +195,6 @@ def non_max_suppression2(prediction, num_classes,max_box,conf_thres=0.5, nms_thr
     elif output[0].shape[0]>max_box:
         conf_thres_5=(output[0][:,4]*output[0][:,5]).topk(max_box,axis=0)
         return output, conf_thres_5[0][max_box-1].item()
-    # else:
-    #     return non_max_suppression2(prediction, num_classes, conf_thres=conf_thres-0.1, nms_thres=0.4,is_resize=True)
+
 
 
